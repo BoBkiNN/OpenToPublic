4825b3f923137bd88e114c07ea995301fccf9714
BoBkiNN <78136575+BoBkiNN@users.noreply.github.com>
2025-06-24 17:40:16 +0500

    better compat with other mods that change minecraft title

--- common/src/main/java/xyz/bobkinn/opentopublic/mixin/MixinMinecraft.java
+++ common/src/main/java/xyz/bobkinn/opentopublic/mixin/MixinMinecraft.java
@@ -1,18 +1,11 @@
 package xyz.bobkinn.opentopublic.mixin;
 
-import com.mojang.blaze3d.platform.Window;
-import net.minecraft.SharedConstants;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.multiplayer.ClientPacketListener;
 import net.minecraft.client.resources.language.I18n;
-import net.minecraft.client.server.IntegratedServer;
-import org.jetbrains.annotations.Nullable;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import xyz.bobkinn.opentopublic.OpenMode;
 import xyz.bobkinn.opentopublic.OpenToPublic;
@@ -23,12 +16,6 @@ import java.io.File;
 
 @Mixin(Minecraft.class)
 public abstract class MixinMinecraft {
-
-    @Shadow
-    private @Nullable IntegratedServer singleplayerServer;
-    @Shadow
-    @Final
-    private Window window;
 
     @Inject(method = "run", at = @At("HEAD"))
     public void run(CallbackInfo ci) {
@@ -47,41 +34,20 @@ public abstract class MixinMinecraft {
         OpenToPublic.updateConfig(OpenToPublic.modConfigPath.resolve("config.json"));
     }
 
-    @Shadow
-    public abstract @Nullable ClientPacketListener getConnection();
-
-    @Shadow public abstract boolean isConnectedToRealms();
-
-    @Inject(method = "updateTitle", at = @At("RETURN"))
-    public void onUpdateWindowTitle(CallbackInfo ci) {
-        this.window.setTitle(openToPublic$getTitle());
+    // we don't use ordinal here because some other mods can add other calls
+    @Redirect(method = "createTitle", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/resources/language/I18n;get(Ljava/lang/String;[Ljava/lang/Object;)Ljava/lang/String;"))
+    public String onWindowTitleLanBranch(String key, Object[] args) {
+        if (!key.equals("title.multiplayer.lan")) { // process other calls as usual
+            return I18n.get(key, args);
+        }
+        // now overriding only lan
+        if (OpenToPublic.openedMode == OpenMode.UPNP) {
+            return I18n.get("opentopublic.title.multiplayer.upnp");
+        } else if (OpenToPublic.openedMode == OpenMode.MANUAL) {
+            return I18n.get("opentopublic.title.multiplayer.wan");
+        } else {
+            return I18n.get("title.multiplayer.lan");
+        }
     }
 
-    @Unique
-    public String openToPublic$getTitle() {
-        StringBuilder stringBuilder = new StringBuilder("Minecraft");
-        if (Minecraft.checkModStatus().shouldReportAsModified()) {
-            stringBuilder.append("*");
-        }
-        stringBuilder.append(" ");
-        stringBuilder.append(SharedConstants.getCurrentVersion().getName());
-        ClientPacketListener clientPlayNetworkHandler = this.getConnection();
-        if (clientPlayNetworkHandler != null && clientPlayNetworkHandler.getConnection().isConnected()) {
-            stringBuilder.append(" - ");
-            if (this.singleplayerServer != null && !this.singleplayerServer.isPublished() || OpenToPublic.openedMode == null) {
-                stringBuilder.append(I18n.get("title.singleplayer"));
-            } else if (isConnectedToRealms()) {
-                stringBuilder.append(I18n.get("title.multiplayer.realms"));
-            } else if (OpenToPublic.openedMode == OpenMode.UPNP) {
-                stringBuilder.append(I18n.get("opentopublic.title.multiplayer.upnp"));
-            } else if (OpenToPublic.openedMode == OpenMode.MANUAL) {
-                stringBuilder.append(I18n.get("opentopublic.title.multiplayer.wan"));
-            } else if (OpenToPublic.openedMode == OpenMode.LAN) {
-                stringBuilder.append(I18n.get("title.multiplayer.lan"));
-            } else {
-                stringBuilder.append(I18n.get("title.multiplayer.other"));
-            }
-        }
-        return stringBuilder.toString();
-    }
 }
